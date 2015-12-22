d9d77396a30f2bfd3375064fba3f9f27c4a7e1ea
Alexey S <stralex@gmail.com>
2015-12-22 13:54:02 +0300

    Update rand_300k.py

--- rand_300k.py
+++ rand_300k.py
@@ -2,13 +2,24 @@
 
 from random import shuffle
 from sys import stdout
+from time import sleep
 from netaddr import *
 
 ip=IPNetwork('64.0.0.0/3')
 subnets=list(ip.subnet(22))
 shuffle(subnets)
 count = 0
+c=0
 while (count < 300000):
-   stdout.write( 'announce route ' + str(subnets[count]) + ' next-hop self\n')
-   stdout.flush()
-   count = count + 1
+        while (c<1000):
+                stdout.write( 'announce route ' + str(subnets[count]) + ' next-hop self\n')
+                count = count + 1
+                c=c+1
+        stdout.flush()
+        sleep(5)
+        c=0
+        count = count + 1
+
+#Loop endlessly to allow ExaBGP to continue running
+while True:
+    sleep(1)
